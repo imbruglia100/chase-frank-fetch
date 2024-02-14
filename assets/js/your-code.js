ddb694545e1a373d0567c0e6385fba7a9c8d3d95
Frank <imbruglia100@gmail.com>
2024-02-13 19:02:57 -0500

    d

--- assets/js/your-code.js
+++ assets/js/your-code.js
@@ -1,19 +1,55 @@
 export function getAllDogs() {
-    // Your code here 
+    return fetch("/dogs")
 }
 
 export function getDogNumberTwo() {
-    // Your code here 
+    return fetch("/dogs/2")
 }
 
 export function postNewDog() {
-    // Your code here 
+    const url = "/dogs"
+
+    const body = new URLSearchParams({
+        name: "jim",
+        age: 13
+    })
+
+    const headers = {
+        "Content-Type": "application/x-www-form-urlencoded",
+    }
+
+    const options = {
+         headers: headers,
+         method: "POST",
+         body: body
+        }
+
+    return fetch(url, options)
 }
 
 export function postNewDogV2(name, age) {
-     // Your code here 
+    const url = "/dogs"
+
+    const body = new URLSearchParams({
+        name,
+        age
+    })
+
+    const headers = {
+        "Content-Type": "application/x-www-form-urlencoded",
+    }
+
+    const options = {
+         headers: headers,
+         method: "POST",
+         body: body
+        }
+
+    return fetch(url, options)
 }
 
 export function deleteDog(id) {
-      // Your code here 
+    
+
+      return fetch("/dogs", options)
 }
